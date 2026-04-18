c3d5c43afe339c50738645a89ca23b644dc7bd3d
notebook <martinvojtek03@gmail.com>
2026-04-18 19:59:12 +0200

    Přidání stránky s nastavením

---
+++ Stin-Semestral/Views/Settings/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stin_Semestral.Views.Settings
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
